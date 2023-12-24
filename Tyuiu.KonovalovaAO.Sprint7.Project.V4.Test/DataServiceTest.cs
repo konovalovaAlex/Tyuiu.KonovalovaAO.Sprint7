c57a56371832b6de7d73feef99a5717242a11329
konovalovaAlex <144355131+konovalovaAlex@users.noreply.github.com>
2023-12-24 16:12:39 +0500

    Add project files.

---
+++ Tyuiu.KonovalovaAO.Sprint7.Project.V4.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Tyuiu.KonovalovaAO.Sprint7.Project.V4.Lib;
+namespace Tyuiu.KonovalovaAO.Sprint7.Project.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.KonovalovaAO.Sprint7.Project.V4.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KonovalovaAO.Sprint7.Project.V4.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KonovalovaAO.Sprint7.Project.V4.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d5a30004-5393-4a9e-891e-5bc6f95d2ad6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
